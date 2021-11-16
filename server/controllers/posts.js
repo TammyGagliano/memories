ba591ba3eb0327d499fb220a77df8fd7a5081aca
Tammy Gagliano <tracsd@gmail.com>
2021-11-16 16:20:33 -0600

    update files to add create post

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,3 +1,27 @@
-export const getPosts = (req, res) => {
-    res.send('This Works!');
+import PostMessage from '../models/postMessage.js';
+
+export const getPosts = async (req, res) => {
+    // if everything is successful
+    // we want to retrieve all the posts we have in th database
+    try {
+        const postMessages = await PostMessage.find();
+        // console.log(postMessages);
+        res.status(200).json(postMessages);
+    // this will happen if we get an error
+    } catch (error ){
+        res.status(404).json({ message: error.message });
+        console.log();
+    }
+}
+
+export const createPost = async (req, res) => {
+    const post = req.body;
+    const newPost = new PostMessage(post);
+    
+    try {
+        await newPost.save();
+        res.status(201).json(newPost);
+    } catch (error ){
+        res.status(409).json({ message: error.message });
+    }
 }
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import { getPosts } from '../controllers/posts.js'
+import { getPosts, createPost  } from '../controllers/posts.js'
+import PostMessage from '../models/postMessage.js';
 // set up router
 const router = express.Router();
 
 // Start adding routes // localhost:5000/posts
 router.get('/', getPosts);
+router.post('/', createPost);
 
 export default router; 
